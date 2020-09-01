214256f8099838f71d897d59936e30b5fadcfbd3
HelloRocket <justin@hellorocket.org>
2020-09-01 19:56:32 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,5 +5,6 @@ input.onButtonPressed(Button.B, function () {
     Hero.change(LedSpriteProperty.X, 1)
 })
 let Hero: game.LedSprite = null
+let Wall: number[] = []
 Hero = game.createSprite(2, 4)
 Hero.set(LedSpriteProperty.Blink, 300)
